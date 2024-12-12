59de5565011d4bd7c3ea06c0746f363285b1f326
eecheveard <eecheveard1@educacion.navarra.es>
2024-12-12 20:19:50 +0100

    hjkhjk

--- Ejercicio12.py
+++ Ejercicio12.py
@@ -8,22 +8,23 @@ def calcular_desviacion_tipica(lista_numeros, media):
     return math.sqrt(varianza)
 
 def main():
-    # Solicitar al usuario una muestra de números
+    
     entrada = input("Introduce una lista de números separados por comas: ")
     
     try:
-        # Convertir la entrada en una lista de números
+        
         numeros = [float(numero) for numero in entrada.split(",")]
         
-        # Calcular la media y la desviación típica
+        
         media = calcular_media(numeros)
         desviacion_tipica = calcular_desviacion_tipica(numeros, media)
         
-        # Mostrar los resultados
+        
         print(f"Media: {media:.2f}")
         print(f"Desviación típica: {desviacion_tipica:.2f}")
     except ValueError:
         print("Por favor, introduce únicamente números separados por comas.")
 
 if __name__ == "__main__":
+    
     main()
